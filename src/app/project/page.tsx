16748fde21c34b52faf69516cc151e1a8b9abbb1
Nida <nnpervez333@gmail.com>
2024-09-11 11:01:35 -0700

    change

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -66,7 +66,6 @@ const Project = () => {
             </div>
           </div>
         </div>
-        
 
 
         <div className="lg:w-1/3 sm:w-1/2 p-4">
@@ -197,6 +196,59 @@ const Project = () => {
               </p>
               <Link target="_blank"
               href={"http://nav-glonf74bu-nida-pervezs-projects.vercel.app"}>
+              <p className="leading-relaxed text-indigo-500 mb-1 hover:underline">
+              View Project</p> </Link >
+            
+            </div>
+          </div>
+        </div>
+
+
+        <div className="lg:w-1/3 sm:w-1/2 p-4">
+          <div className="flex relative">
+          <Image
+              alt="gallery"
+              className="absolute inset-0 w-full h-full object-cover object-center"
+              src={require('../../../public/cv.jpg')}
+            />
+            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+              <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                CV Project
+              </h2>
+              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+              Milestone 1 and 2
+              </h1>
+              <p className="leading-relaxed line-clamp-2">
+              This website project pertains to the curriculum vitae 
+              </p>
+              <Link target="_blank"
+              href={"https://hackathon-milestone1-2-hazel.vercel.app/"}>
+              <p className="leading-relaxed text-indigo-500 mb-1 hover:underline">
+              View Project</p> </Link >
+            
+            </div>
+          </div>
+        </div>
+
+        <div className="lg:w-1/3 sm:w-1/2 p-4">
+          <div className="flex relative">
+          <Image
+              alt="gallery"
+              className="absolute inset-0 w-full h-full object-cover object-center"
+              src={require('../../../public/resumebuilder.jpg')}
+            />
+            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+              <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                Dynamic Builder Project
+              </h2>
+              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+              Resume Builder
+              </h1>
+              <p className="leading-relaxed line-clamp-2">
+              This project is focused on creating and enhancing resumes.
+              </p>
+              <Link target="_blank"
+              href={"https://hackathon-milestone3-4-5.vercel.app/"}>
               <p className="leading-relaxed text-indigo-500 mb-1 hover:underline">
               View Project</p> </Link >
             
